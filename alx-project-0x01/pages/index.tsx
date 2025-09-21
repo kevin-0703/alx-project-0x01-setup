8f7666ffa88f28e7a095505b692ad0e2ee77ccd9
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-21 14:07:04 +0200

    setting up navigation between pages

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
+      <Header />
       <h1 className="text-7xl font-thin">Welcome Page</h1>
     </div>
   );
