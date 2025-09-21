8a7f96409527b6940318253c23d81d559eb661af
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-21 16:44:46 +0200

    implementing a USERCARD component

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,26 +4,51 @@ export interface PostProps {
   title: string;
   body: string;
 }
+
 export interface UserProps {
-  id: 1;
-  name: "Leanne Graham";
-  username: "Bret";
-  email: "Sincere@april.biz";
+  id: number;
+  name: string;
+  username: string;
+  email: string;
   address: {
-    street: "Kulas Light";
-    suite: "Apt. 556";
-    city: "Gwenborough";
-    zipcode: "92998-3874";
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
     geo: {
-      lat: "-37.3159";
-      lng: "81.1496";
+      lat: string;
+      lng: string;
     };
   };
-  phone: "1-770-736-8031 x56442";
-  website: "hildegard.org";
+  phone: string;
+  website: string;
   company: {
-    name: "Romaguera-Crona";
-    catchPhrase: "Multi-layered client-server neural-net";
-    bs: "harness real-time e-markets";
+    name: string;
+    catchPhrase: string;
+    bs: string;
   };
 }
+
+const user: UserProps = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
+};
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,31 @@
+// pages/users/index.tsx
 import React from "react";
+import { UserProps } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
+
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {posts.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
