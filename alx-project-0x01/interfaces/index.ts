76237a62ac6e89e28235c1f4e0ac684c414ca02b
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-21 16:58:14 +0200

    extending post page

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -52,3 +52,14 @@ const user: UserProps = {
     bs: "harness real-time e-markets",
   },
 };
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
